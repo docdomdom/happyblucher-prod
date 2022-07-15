ef6fd33f3a0a954b9c08b30fd85363b7f6d710a8
docdom <sonicblue@posteo.de>
2022-07-15 09:51:10 +0200

    env geheim-schluesl

--- blucher2/settings.py
+++ blucher2/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'DswnfaLIvt9XDLXvtQkSkM5PBB3oO9Vdkm2phyIZAp0B5v5z25o'
 
 
 env = environ.Env()
